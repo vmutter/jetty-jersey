0924cc7b7d343616ec021ce1ab4f173cb33143d3
Vinicius Mutter Erbetta <verbetta@ciandt.com>
2015-10-08 10:09:26 -0300

    POM and first start

--- src/main/java/com/vmutter/jettyjersey/rs/HelloWorldRS.java
+++ src/main/java/com/vmutter/jettyjersey/rs/HelloWorldRS.java
@@ -4,14 +4,14 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
+@Path("/rs")
 public class HelloWorldRS {
 
 	@GET
-	@Path("/hello")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response helloWorld() {
-		return Response.ok("Hello World").build();
+	@Path("hello")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String helloWorld() {
+		return "oi"; // Response.ok("Hello World").build();
 	}
 }
